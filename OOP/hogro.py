db002c10b1df9690c2fd7ff646406dcaeb7b4ae5
fernandacastro-lang <fernanda.castro.lazaro@gmail.com>
2026-02-26 18:51:33 -0800

    realice un nuevo codigo en el documento ogro

---
+++ OOP/hogro.py
@@ -0,0 +1,9 @@
+from enemigo import *
+
+class ogro(enemigo):
+    def _init_(self, puntos_energia=20, ataque=3):
+        super()._init_(tipo_enemigo='ogro', puntos_energia=puntos_energia, ataque=ataque)
+
+        def habla(self):
+            print("ogro aplasta todo!!!")
+            
